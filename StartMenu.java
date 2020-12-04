907f27cddcad70361f00587921f89d4c09f3a1ba
laronichill <74273468+laronichill@users.noreply.github.com>
2020-12-03 20:44:37 -0500

    Graphics Update
    Dosen't include gameplay changes

--- StartMenu.java
+++ StartMenu.java
@@ -1,21 +1,23 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class StartMenu here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class StartMenu extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class StartMenu.
      * 
      */
-    public MyWorld()
+    public StartMenu()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 360, 1); 
+        
+        
     }
 }
